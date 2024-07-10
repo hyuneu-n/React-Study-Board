3299956601fa0aebf1cf13d7e1d0068ddf4f0b85
JJERY <hyuneun@skuniv.ac.kr>
2024-07-10 16:04:08 +0900

    modify App.js

--- my-board/src/App.js
+++ my-board/src/App.js
@@ -2,9 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let posts = 'Sample Sentence';
   return (
     <div className="App">
-      
+      <div className='black-nav'>
+        <div>Sample</div>
+      </div>
+      <h4>{ posts }</h4>
+    <div className="list">
+      <h3>{ posts }</h3>
+      <p>2024.7.10</p>
+      <hr/>
+    </div>
     </div>
   );
 }
