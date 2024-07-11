d59cae25e3c4bc5e5994406deb9fe347a81c8543
JJERY <hyuneun@skuniv.ac.kr>
2024-07-11 16:57:59 +0900

    Update

--- my-board/src/App.js
+++ my-board/src/App.js
@@ -1,20 +1,55 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Modal from 'react-modal';
 
 function App() {
-  let posts = 'Sample Sentence';
+  let data;
+  let [title, changeTitle] = useState(['Sample Sentence 1', 'Sample Sentence 2', 'Sample Sentence 3']);
+  let [like, likeFunction] = useState(0);
+  let [bad, badFunction] = useState(0);
+  let [showUI, UIFunc] = useState(false);
+  let [modal, modalFunc] = useState(false);
+
   return (
+    <>
     <div className="App">
-      <div className='black-nav'>
-        <div>Sample</div>
-      </div>
-      <h4>{ posts }</h4>
+    	<div className="black-nav">
+    		<div>TITLE</div>
+    		<div> { data } </div>
+	  </div>
+    </div>
     <div className="list">
-      <h3>{ posts }</h3>
-      <p>2024.7.10</p>
-      <hr/>
+        <h3> {title[0]} <span onClick={() => { likeFunction(like + 1) }}> 👍🏻 </span> {like}</h3>
+        <p>24.07.09</p>
+        <hr />
+      </div>
+
+      <div className="list">
+        <h3> {title[1]} <span onClick={() => { badFunction(bad - 1) }}> 👎🏻 </span> {bad}</h3>
+        <p>24.07.10</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {title[2]} </h3>
+        <p>24.07.11</p>
+        <hr/>
+      </div>
+      <div>
+        
+      {
+      modal === true
+      ? <Modal />
+      : null}
+    <button onClick={() => { UIFunc(true)} }>Open</button>
+    <button onClick={() => {modalFunc(!modal)}}>Btn</button>
+      {
+          modal === true
+          ? <Modal />
+          : null
+      }
     </div>
-    </div>
+    </>
   );
 }
 
